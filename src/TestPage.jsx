5a407c48a854494f492a543148a682d57c5940b5
jeremycmdr <115502515+jeremycmdr@users.noreply.github.com>
2024-02-27 20:22:22 +0100

    added loader

--- src/TestPage.jsx
+++ src/TestPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Loader from "./Loader";
 import "./TestPage.css";
 
 const TestPage = () => {
@@ -7,15 +8,18 @@ const TestPage = () => {
   const [cancelToken, setCancelToken] = useState(null);
   const [showData, setShowData] = useState("");
   const [cancelMessage, setCancelMessage] = useState("");
+  const [loadedData, setLoadedData] = useState(false);
 
   const openingPopup = () => {
-    setShowData("");
-    setCancelMessage("");
     if (!openPopup) {
+      setLoadedData(true);
+      setShowData("");
+      setCancelMessage("");
       const source = axios.CancelToken.source();
       setCancelToken(source);
 
       setTimeout(() => {
+        //setTimeout just to simulate time loading
         axios
           .post(
             "https://jsonplaceholder.typicode.com/posts",
@@ -31,6 +35,7 @@ const TestPage = () => {
           .then((response) => {
             if (response) {
               setShowData(JSON.stringify(response.data));
+              setLoadedData(false);
             }
           })
           .catch((error) => {
@@ -41,13 +46,12 @@ const TestPage = () => {
             }
           });
       }, 2000);
-    } else {
-      console.log("else");
     }
     setOpenPopup(true);
   };
 
   const closePopup = () => {
+    setLoadedData(false);
     setShowData("");
     setOpenPopup(false);
     if (cancelToken) {
@@ -63,14 +67,14 @@ const TestPage = () => {
         className="start_request"
         onClick={openingPopup}
       >
-        Open Pop-up
+        Run Axios
       </div>
       <div
         style={{ backgroundColor: "red" }}
         className="start_request"
         onClick={closePopup}
       >
-        Close Pop-up
+        Cancel Axios
       </div>
       {showData !== "" && (
         <div className="messages_text response_txt">{showData}</div>
@@ -78,6 +82,7 @@ const TestPage = () => {
       {cancelMessage !== "" && (
         <div className="messages_text error_txt">{cancelMessage}</div>
       )}
+      {loadedData && <Loader></Loader>}
     </>
   );
 };
